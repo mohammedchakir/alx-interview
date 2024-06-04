f3ce5f4439f7af72c4ce0956d1c12224df21c5b5
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-04 13:11:50 +0100

    commit corrections and stand for check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,21 +3,40 @@
 
 const request = require('request');
 const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a Movie ID as the first argument');
+  process.exit(1);
+}
+
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach(character => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error('Error fetching the movie details:', err);
+    process.exit(1);
   }
+
+  if (res.statusCode !== 200) {
+    console.error('Failed to fetch movie details. Status code:', res.statusCode);
+    process.exit(1);
+  }
+
+  const characters = body.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        console.error('Error fetching character details:', err);
+        return;
+      }
+
+      if (res.statusCode !== 200) {
+        console.error('Failed to fetch character details. Status code:', res.statusCode);
+        return;
+      }
+
+      console.log(body.name);
+    });
+  });
 });
