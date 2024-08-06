b5760324e95c2405d0766cd6a48f333c2f80738c
mohammedchakir <zerroukimedchakir@gmail.com>
2024-08-06 07:48:25 +0100

    add for check

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Prime Game."""
+""" Module for solving prime game question """
 
 
 def sieve(n):
-    """Returns a list of prime numbers up to n."""
+    """
+    Returns an array where index indicates if the number
+    is prime (True) or not (False)
+    """
     is_prime = [True] * (n + 1)
     p = 2
     while p * p <= n:
@@ -11,33 +14,41 @@ def sieve(n):
             for i in range(p * p, n + 1, p):
                 is_prime[i] = False
         p += 1
-    return [p for p in range(2, n + 1) if is_prime[p]]
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+    return is_prime
+
+
+def prime_count_up_to(n, is_prime):
+    """
+    Returns an array where index contains the count of prime
+    numbers up to that index
+    """
+    prime_count = [0] * (n + 1)
+    count = 0
+    for i in range(n + 1):
+        if is_prime[i]:
+            count += 1
+        prime_count[i] = count
+    return prime_count
 
 
 def isWinner(x, nums):
-    """Determine the winner of each game and return the overall winner."""
-    if x < 1 or not nums:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
+
+    max_num = max(nums)
+    is_prime = sieve(max_num)
+    prime_count = prime_count_up_to(max_num, is_prime)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes = sieve(n)
-        if not primes:
-            ben_wins += 1
-            continue
-
-        move_count = 0
-        while primes:
-            smallest_prime = primes[0]
-            primes = [num for num in primes if num % smallest_prime != 0]
-            move_count += 1
-
-        if move_count % 2 == 0:
-            ben_wins += 1
-        else:
+        if prime_count[n] % 2 == 1:
             maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
@@ -45,3 +56,7 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
